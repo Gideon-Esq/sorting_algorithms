5c604b631b91cdff5b2caedd4b8c550ffa975d01
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-05-05 10:55:13 -0700

    Sorts an array of integers in ascending order using the Bitonic sort algorithm.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,11 +12,11 @@ void quick_sort(int *array, size_t size);
  */
 void swap_ints(int *a, int *b)
 {
-  int tmp;
+	int tmp;
 
-  tmp = *a;
-  *a = *b;
-  *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -31,29 +31,29 @@ void swap_ints(int *a, int *b)
  */
 int lomuto_partition(int *array, size_t size, int left, int right)
 {
-  int *pivot, above, below;
+	int *pivot, above, below;
 
-  pivot = array + right;
-  for (above = below = left; below < right; below++)
-    {
-      if (array[below] < *pivot)
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
 	{
-	  if (above < below)
-	    {
-	      swap_ints(array + below, array + above);
-	      print_array(array, size);
-	    }
-	  above++;
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap_ints(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
 	}
-    }
 
-  if (array[above] > *pivot)
-    {
-      swap_ints(array + above, pivot);
-      print_array(array, size);
-    }
+	if (array[above] > *pivot)
+	{
+		swap_ints(array + above, pivot);
+		print_array(array, size);
+	}
 
-  return (above);
+	return (above);
 }
 
 /**
@@ -67,14 +67,14 @@ int lomuto_partition(int *array, size_t size, int left, int right)
  */
 void lomuto_sort(int *array, size_t size, int left, int right)
 {
-  int part;
+	int part;
 
-  if (right - left > 0)
-    {
-      part = lomuto_partition(array, size, left, right);
-      lomuto_sort(array, size, left, part - 1);
-      lomuto_sort(array, size, part + 1, right);
-    }
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
 }
 
 /**
@@ -88,8 +88,8 @@ void lomuto_sort(int *array, size_t size, int left, int right)
  */
 void quick_sort(int *array, size_t size)
 {
-  if (array == NULL || size < 2)
-    return;
+	if (array == NULL || size < 2)
+		return;
 
-  lomuto_sort(array, size, 0, size - 1);
+	lomuto_sort(array, size, 0, size - 1);
 }
