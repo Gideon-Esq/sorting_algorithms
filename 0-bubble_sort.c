c7b27aa865dd26164fc0b6de67517e7647bca756
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-05-05 13:22:53 -0700

    Opé o

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers.
+ * @b: The first integer parameter to swap.
+ * @c: The second integer parameter to swap.
+ */
+void swap_ints(int *b, int *c)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *c;
+	*c = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
